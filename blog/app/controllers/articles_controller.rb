a29c780f8e68656eeef4ad955cedaed4597f3692
mdlawrenceusa <mdlawrence@me.com>
2017-03-20 10:36:12 -0400

    End of Class - Edit
    Competed Edit will start with delete next class.

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def index
     @articles = Article.all
   end
@@ -20,6 +24,17 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
 
   private
   def article_params
