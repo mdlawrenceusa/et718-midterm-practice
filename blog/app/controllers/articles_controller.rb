f37f568783ad4c3195fc6a92a809976e466738d1
mdlawrence <mdlawrenceusa@gmail.com>
2017-03-22 11:09:40 -0400

    Ready for the second table
    Competed refactroing

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,28 +1,26 @@
 class ArticlesController < ApplicationController
+
   def new
     @article = Article.new
-  end
-
-  def edit
-    @article = Article.find(params[:id])
   end
 
   def index
     @articles = Article.all
   end
 
-  def show
+  def edit
     @article = Article.find(params[:id])
   end
 
-  def create
-    @article = Article.new(article_params)
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
 
-    if @article.save
-      redirect_to @article
-    else
-      render 'new'
-    end
+    redirect_to articles_path
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
 
   def update
@@ -35,10 +33,18 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
 
   private
   def article_params
     params.require(:article).permit(:title, :text)
   end
-
 end
